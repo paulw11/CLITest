6834aaf4ffb7f49fe6c7b7554b16c0226799ed64
Paul Wilkinson <paulw@wilko.me>
2018-09-22 08:27:30 +1000

    Initial Commit

---
+++ CLITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CLITest
+//
+//  Created by Paul Wilkinson on 22/9/18.
+//  Copyright © 2018 Paul Wilkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
